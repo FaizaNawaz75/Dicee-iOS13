feffbcd23edc52766c307bd0a01c3ec3ff12c2d1
Faiza Nawaz <Faizanawaz755@gmail.com>
2023-03-10 13:47:13 +0500

    Updated the UI and tried random functions

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -2,19 +2,30 @@
 //  ViewController.swift
 //  Dicee-iOS13
 //
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        rollDice()
+    }
+    
+    private func rollDice(){
+        
+        let diceNames = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+        
+        diceImageViewOne.image = UIImage(imageLiteralResourceName: diceNames.randomElement()!)
+        diceImageViewTwo.image = UIImage(imageLiteralResourceName: diceNames[Int.random(in: 0...5)])
+        
+    }
+    
 }
 
